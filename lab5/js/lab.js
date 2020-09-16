72a66676e77765b63fb36cd74384f8fbcdb4a98b
ajsamudio <70294918+ajsamudio@users.noreply.github.com>
2020-09-16 12:29:08 -0700

    Lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+/**
+ * Author:    AJ Samudio
+ * Created:   09.16.2020
+ *
+ *
+ **/
+
+make = "kia";
+model = "forte";
+color = "black";
+year = 2015;
+age = 2020 - year;
+ownIt = true;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
