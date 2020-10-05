63e9cb895f227ed9fb490f8dc7650035c258c46c
ajsamudio <70294918+ajsamudio@users.noreply.github.com>
2020-10-05 12:31:30 -0700

    Lab10

---
+++ lab10/js/lab.js
@@ -0,0 +1,26 @@
+/**
+ * Author:    AJ Samudio
+ * Created:   10.05.2020
+ *
+ * (c) Copyright by AJ S.
+ **/
+ var button = document.getElementById("myButton");
+ var scan = document.getElementById("fname");
+ var text = document.getElementById("output");
+
+
+
+ function buttonPress() {
+
+   var inputText = scan.value;
+
+   text.innerText = "Hello, " + inputText;
+
+   // var nameArray = scan.sort().reverse();
+   // text.innerText = "Hello, " + nameArray;
+   fname.value = "";
+
+
+  fname.focus();
+}
+button.addEventListener("click", buttonPress);
