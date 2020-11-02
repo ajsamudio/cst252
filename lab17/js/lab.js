1cb7e52dbceecc45556f702fd475005afb34232e
ajsamudio <70294918+ajsamudio@users.noreply.github.com>
2020-11-02 11:48:01 -0800

    lab17

--- lab17/js/lab.js
+++ lab17/js/lab.js
@@ -4,3 +4,31 @@
  *
  * (c) Copyright by AJ S.
  **/
+var buttonA = $("#Chabutton");
+var buttonB = $("#Probutton");
+var buttonC = $("#Resbutton");
+
+var chal = $("#Challenge");
+var prob = $("#Problem");
+var res = $("#Results");
+
+buttonA.click(function(){
+
+	chal.addClass("green-button");
+  alert("Hello! I am an alert box!!");
+
+})
+
+buttonB.click(function(){
+
+	prob.addClass("green-button");
+  alert("Hello! I am an alert box!!");
+
+})
+
+buttonC.click(function(){
+
+	res.addClass("green-button");
+  alert("Hello! I am an alert box!!");
+
+})
